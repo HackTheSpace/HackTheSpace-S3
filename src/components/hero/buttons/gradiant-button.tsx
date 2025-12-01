adada0f411f9f0dad556497efbe7a36466c3f3b0
Akash-Sathi <akash@softwaresathi.in>
2025-12-01 21:10:12 +0530

    Add Link for buttons

--- src/components/hero/buttons/gradiant-button.tsx
+++ src/components/hero/buttons/gradiant-button.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-
+"use client"
 type Props = {
     content: string;
     className?: string;
+    onClick?: () => void;
+    href?: string;
+    target?: string;
 }
 
-const GradiantButton = ({content,className}: Props) => {
+const GradiantButton = ({content,className,onClick,href,target}: Props) => {
     return (
-        <div className="p-px rounded-lg bg-gradient-brand inline-block h-fit">
-            <button className={`bg-[#FFF9F3] rounded-lg py-2 px-6 text-black font-poppins font-medium cursor-pointer ${className}`}>
+        <a href={href || "#"} target={target || "_self"} className="p-px rounded-lg bg-gradient-brand inline-block h-fit">
+            <button type="button" onClick={onClick} className={`bg-[#FFF9F3] rounded-lg py-2 px-6 text-black font-poppins font-medium cursor-pointer ${className}`} onClick={onClick}>
                 {content}
             </button>
-        </div>
+        </a>
     )
 }
 
--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+"use client"
 import GradiantButton from "./buttons/gradiant-button";
 import AnimatedButton from "./buttons/animated-button";
 
@@ -30,7 +30,7 @@ const Hero = (props: Props) => {
               Every once in a while, a new technology, an old problem, and a big
               idea turn into an innovation.
             </span>{" "}<br/>
-            <div className="italic w-full text-right px-1">- Dean Kamen</div> <br/>
+            <span className="italic block w-full text-right px-1">- Dean Kamen</span> <br/>
             <span className="text-xs md:text-sm lg:text-base">
             What begins here doesn’t end here… it  <span className="italic">launches </span>forward, <span className="font-bold">welcome in!</span>
             </span>
@@ -40,11 +40,11 @@ const Hero = (props: Props) => {
           id="hero-buttons"
           className="z-10 flex justify-between flex-col md:flex-row items-center gap-4 "
         >
-          <GradiantButton content="Pre-Register" />
+          <GradiantButton content="Pre-Register" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTY_Nv3eAi0u5UrtLjakjW0OJEbVzX00fj-ZciY0-oDtff2Q/viewform"/>
           <AnimatedButton className="z-10">
-            <GradiantButton content="Sponsor Us" className="b-0" />
+            <GradiantButton content="Sponsor Us" className="b-0" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdIO6KIM825CeSJTb70R7kqyCLRtFsLfnHRx9nEH7yC9lH9Ew/viewform"/>
           </AnimatedButton>
-          <GradiantButton content="Join Discord" />
+          <GradiantButton content="Join Discord" target="_blank" href="https://discord.com/invite/FJKTSgdxPX"/>
         </section>
 
         <div className="text-center font-qurova text-md md:text-2xl animate-pulse text-gray-400 font-light">
