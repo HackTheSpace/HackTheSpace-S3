ecb7d76d776b73b628583b3dcb6122b61cee9bab
Akash-Sathi <akash@softwaresathi.in>
2025-12-02 23:42:51 +0530

    Fix for iphone

--- src/components/hero/buttons/gradiant-button.tsx
+++ src/components/hero/buttons/gradiant-button.tsx
@@ -10,7 +10,7 @@ type Props = {
 const GradiantButton = ({content,className,onClick,href,target}: Props) => {
     return (
         <a href={href || "#"} target={target || "_self"} className="p-px rounded-lg bg-gradient-brand inline-block h-fit">
-            <button type="button" onClick={onClick} className={`bg-[#FFF9F3] rounded-lg py-2 px-6 text-black font-poppins font-medium cursor-pointer ${className}`} onClick={onClick}>
+            <button type="button" onClick={onClick} className={`bg-[#FFF9F3] rounded-lg py-2 px-6 text-black font-poppins font-medium cursor-pointer ${className}`}>
                 {content}
             </button>
         </a>
